010ae70235c43eef3044fd182b9b25fd9422f141
pohmelie <pohmelie@mail.ru>
2013-01-09 17:26:08 +0200

    * research

--- recipe.py
+++ recipe.py
@@ -7,3 +7,11 @@ def rev(x):
         return bytes().fromhex(x.replace(" ", ""))
     else:
         return " ".join(map("{:0>2x}".format, x))
+
+if __name__ == "__main__":
+    print(rev(b'\xa1\xd9\x00(H@\xf0\xb0\x00(k\x06'))
+    print(rev(b'\xa1\xe9\x00\x90\xe0\xc80\xb0\x00(\xdc\x02'))
+    print(rev(b'\xa1\xd1\x0008\xc8\xe8\xb0\x00\x187\x05'))
+    print()
+    print(rev(b'!28\xc8\xe8\x00\x00\x00\x00'))
+    print(rev(b'!*H@\x00\x00\x00\x00'))
