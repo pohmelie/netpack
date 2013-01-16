84f2f624327705b0360d46a8fdac126917878108
pohmelie <pohmelie@mail.ru>
2013-01-16 14:51:47 +0200

    * strip useless code

--- checksum.py
+++ checksum.py
@@ -5,7 +5,7 @@ from construct.protocols.layer4.tcp import tcp_header
 def recalculateIPchecksum(ip):
     ip.header.checksum = 0
     ipbin = layer3_ipv4.build(ip)
-    step = lambda i: ((ipbin[i] << 8) & 0xff00) + (ipbin[i + 1] & 0xff)
+    step = lambda i: (ipbin[i] << 8) + ipbin[i + 1]
     cs = sum(map(step , range(0, ip.header.header_length, 2)))
     while cs >> 16:
         cs = (cs & 0xffff) + (cs >> 16)
@@ -16,7 +16,7 @@ def recalculateTCPchecksum(ip):
     tcp.header.checksum = 0
     tcpbin = tcp_header.build(tcp.header) + tcp.next + bytes((0,))
     tcplength = ip.header.total_length - ip.header.header_length
-    step = lambda i: ((tcpbin[i] << 8) & 0xff00) + (tcpbin[i + 1] & 0xff)
+    step = lambda i: (tcpbin[i] << 8) + tcpbin[i + 1]
     cs = sum(map(step , range(0, ((tcplength + 1) >> 1) << 1, 2)))
     ip2num = lambda x: tuple(map(int, x.split(".")))
     ip2cs = lambda x: ((x[0] + x[2]) << 8) + x[1] + x[3]
